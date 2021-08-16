ee45b96a217eabdfc67a7e429783f654794765c1
ryandeering <ryandeering1@gmail.com>
2021-08-16 23:54:43 +0100

    more cleaning

--- FourthYearProject/wwwroot/js/StripePayment.js
+++
@@ -1,6 +0,0 @@
-﻿redirectToCheckout = function(sessionId) {
-    var stripe = Stripe("pk_test_hpLfBptgL9qbpWeuJgoX3AO500njVt1MOk");
-    stripe.redirectToCheckout({
-        sessionId: sessionId
-    });
-};
--- FourthYearProject/wwwroot/js/common.js
+++
@@ -1,8 +0,0 @@
-﻿window.ShowToastr = (type, message) => {
-    if (type === "success") {
-        toastr.success(message, "Operation Successful", { timeOut: 10000 });
-    }
-    if (type === "error") {
-        toastr.error(message, "Operation Failed", { timeOut: 10000 });
-    }
-}
